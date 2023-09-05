f7b4611411a4652878f43fae0336748312793c63
Swarzstein <zorrillagja@gmail.com>
2023-09-05 16:49:45 -0400

    navbar added

--- db/migrate/20230904222648_create_recipes.rb
+++ db/migrate/20230904222648_create_recipes.rb
@@ -1,9 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.string :preparation_time
-      t.string :cooking_time
+      t.string :name, null: false
+      t.string :preparation_time, null: false
+      t.string :cooking_time, null: false
       t.text :description
       t.boolean :public
       t.references :user, null: false, foreign_key: true
--- db/migrate/20230904222757_create_foods.rb
+++ db/migrate/20230904222757_create_foods.rb
@@ -1,10 +1,10 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.string :name
-      t.string :measurement_unit
-      t.decimal :price
-      t.decimal :quantity
+      t.string :name, null: false
+      t.string :measurement_unit, null: false
+      t.decimal :price, null: false
+      t.decimal :quantity, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

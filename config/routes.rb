28d525e1fc124d7eb5d572b46a650104bdefc425
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-06 14:15:19 -0600

    Set public_recipes as root

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   resources :foods
   resources :recipes
   resources :users
+  resources :public_recipes
 
   #root to users#index acceced as public_recipes
-  root to: 'users#index', as: 'public_recipes'
+  
+  root to: 'public_recipes#index',
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :public_recipes
+  
   
 end
 
